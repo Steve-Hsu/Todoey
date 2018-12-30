8c710a8a8dddee41463239fe3689ac5546fc8dbf
Steve Hsu <stevehsu@stevede-macbook-pro.local>
2018-12-30 16:38:13 +0800

    Complete the encode and decode the plist.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,26 +11,31 @@ import UIKit
 class TodoListViewController: UITableViewController{
     
     var itemArray = [Item]()
-    
-    //The datas container.
-    let defaults = UserDefaults.standard
+    // The fileManager.default is a singleton type method for storing light datas.
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(dataFilePath as Any)
+   
         
         //After you shot down the app, the data now will be stored inside the phone.
         //When you execute the app again, we retrieve the datas from the container and put it to the correct custom objects that operating for the app.
         //The container of the datas is “defaults.array”
         //The custom object is “itemArray”
 //        //Here we used an optional binding to prevent the app crash when the defaults.array is nil.
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items
-        }
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+//            itemArray = items
+//        }
         
         let newItem = Item()
         newItem.title = "Find Mike"
         itemArray.append(newItem)
+        
+        loadItems()
     }
 
     //MARK - Tableview Datasource Methods.
@@ -64,11 +69,12 @@ class TodoListViewController: UITableViewController{
         
        //Here as a switch to decide the itemArray.done is true or false that is used as a signal in other method to deicde if cell display a checkmark or not.
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-
-        tableView.reloadData()
+        
+        saveItems()
         
         //The method display a animation when user deselect a row.
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
     //MARK - Add new items
@@ -82,17 +88,13 @@ class TodoListViewController: UITableViewController{
         //Completion handler, define spec of the button on the Alert and what will happen after you pressed it.
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What will happen once the user clicks the Add Item button on our UIAlert
-            //To add a new member to the allary of itemArray, by deriving the sring from alertTextFiled. i.e. the user type in a data in the TexiFiled in a alert.
+            
+            //To add a new member to array the "itemArray", by deriving the sring from alertTextFiled. i.e. the user type in a data in the TexiFiled in a alert.
             let newItem = Item()
             newItem.title = textFiled.text!
             self.itemArray.append(newItem)
             
-            //The method is to grab data and save it in the data container of tha app.
-            //In here we grab datas from object, the "itemArray", and we give the container a name "TodoListArray"
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            
-          //The method is to update the TableView since we append a new String to the itemArry, that will also let the method "numberOfRowsInSection" add a new cells in the tableView, so we need to reload to update the TableView.
-            self.tableView.reloadData()
+            self.saveItems()
         }
         
         
@@ -107,6 +109,40 @@ class TodoListViewController: UITableViewController{
     }
     
     
+    //MARK - Model Manupulation methods.
+    //The method is to grab data and save it in the data container of tha app.
+    func saveItems() {
+        // the encoder here is a plist type data. it will be used to grab our custom plist datas.
+        let encoder = PropertyListEncoder()
+        
+        //The method is to grab data and save it in the data container of tha app.
+        do {
+            // The object "data" execute to grab and encode the datas. the data source here is the itemArray.
+            let data = try encoder.encode(itemArray)
+            
+            // It designate where sould the data to be saved. here we designate it sould be saved in as the url in the dataFilePath.
+            try data.write(to: dataFilePath!)
+            
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        //The method is to update the TableView since we append a new String to the itemArry, that will also let the method "numberOfRowsInSection" add a new cells in the tableView, so we need to reload to update the TableView.
+        self.tableView.reloadData()
+    }
     
+    
+    // The method is to derieve datas from the memoris.
+    func loadItems() {
+        // as we save the data and encode it. we to decode it before we derieve it back.
+       if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
 }
 
--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
-class Item {
+// the class here with the type "Encodage" means the datas in the class could be encode into the data type of plist or JSON.
+// The class here with the type "Decodable" means the datas in the class could be decode from data type like plist or JSON.
+// Before we store datas we encode it, and before we retrieve datas we decode it.
+class Item: Codable {
     var title : String = ""
     var done : Bool = false
 }
